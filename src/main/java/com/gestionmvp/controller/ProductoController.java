ea5cf36d3b1c18d91a18a05ba940f55949115aa9
idchavez <71288877+idchavez@users.noreply.github.com>
2025-03-27 12:17:08 -0500

    Update ProductoController.java port

--- src/main/java/com/gestionmvp/controller/ProductoController.java
+++ src/main/java/com/gestionmvp/controller/ProductoController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("gestionmvp-app")
-@CrossOrigin(value = "http://localhost:3000")
+@CrossOrigin(value = "http://localhost:8080")
 public class ProductoController {
     private static final Logger logger = LoggerFactory.getLogger(ProductoController.class);
 
